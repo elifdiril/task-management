2db0bb13c24a30c757b72f2010bfebdebd7ff0ed
Elif <elifdiril6@gmail.com>
2023-12-08 20:43:19 +0000

    deleteTask added

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -21,4 +21,9 @@ export class TasksController {
     createTask(@Body() createTaskDto: CreateTaskDto): Task {
         return this.tasksService.createTask(createTaskDto);
     }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
 }
